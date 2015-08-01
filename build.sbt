f06d7f1e9eeddced0020764e2c5610c742670f1b
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-08-01 15:05:11 -0700

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "cmudict"
+
+version := "1.0-SNAPSHOT"
+
+crossScalaVersions := Seq("2.10.6", "2.11.6")
+
+scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation")
---
+++ src/main/scala/cmudict/CmuDict.scala
@@ -0,0 +1,75 @@
+package cmudict
+
+class CmuDict {
+
+  import CmuDict._
+
+  val wordsWithPhones: Vector[(String, String)] = readDict()
+
+  def phonesForWord(w: String): Vector[String] = {
+    val lcw = w.toLowerCase
+    for {
+      (word, phones) <- wordsWithPhones
+      if word == lcw
+    } yield phones
+  }
+
+  def stressForWord(w: String): Vector[String] =
+    for (phones <- phonesForWord(w)) yield stress(phones)
+
+  def syllableCountForWord(w: String): Vector[Int] =
+    for (stress <- stressForWord(w)) yield stress.length
+
+  def rhymingChunksForWord(w: String): Vector[String] =
+    for (phones <- phonesForWord(w)) yield rhymingChunkForPhones(phones)
+
+  def wordsByStress(s: String): Vector[String] = for {
+    (word, phones) <- wordsWithPhones
+    if stress(phones) == s
+  } yield word
+
+  def wordsByRhymingChunk(chunk: String): Vector[String] = for {
+    (word, phones) <- wordsWithPhones
+    if phones endsWith chunk
+  } yield word
+
+
+  def wordsByRhyme(w: String): Vector[String] = for {
+    chunk <- rhymingChunksForWord(w)
+    word <- wordsByRhymingChunk(chunk)
+    if word != w
+  } yield word
+
+}
+
+object CmuDict {
+
+  def readDict(): Vector[(String, String)] = {
+    val stream = getClass.getResourceAsStream("/cmudict-0.7b")
+    val source = io.Source.fromInputStream(stream, "windows-1252")
+    val wordsWithPhones = for {
+      line <- source.getLines()
+      if !line.startsWith(";;;")
+    } yield {
+      val Array(word, phones) = line.split("  ")
+      (word.replaceAll("""\(\d+\)$""", "").toLowerCase, phones)
+    }
+    val results = wordsWithPhones.toVector
+    source.close()
+    results
+  }
+
+  def stress(phones: String): String =
+    phones.replaceAll("""[^012]""", "")
+
+  def rhymingChunkForPhones(phones: String): String = {
+    val chunks = phones.split(" ")
+    val stress = "[12]$".r
+    for {
+      i <- chunks.indices.reverse
+      if stress.findFirstIn(chunks(i)).nonEmpty
+    } return chunks.slice(i, chunks.length).mkString(" ")
+    phones
+  }
+
+}
